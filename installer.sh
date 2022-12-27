c2d53797956cbed3ee8a3a46bf0a32762812eb58
gitmapd <50059062+gitmapd@users.noreply.github.com>
2022-12-27 21:22:12 +0000

    Add files via upload

---
+++ installer.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+unset choice
+die() { echo "$*" >&2; exit 2; }  # complain to STDERR and exit with error
+needs_arg() { if [ -z "$OPTARG" ]; then die "No arg for --$OPT option"; fi; }
+
+github_api="https://api.github.com"
+
+repo="repos/CanastaWiki/Canasta-CLI/git/refs/tags"
+
+data=$(curl ${github_api}/${repo} 2>/dev/null)
+
+refs=$(jq -r '.. | select(.ref?) | .ref' <<< "${data}")
+versions=( $(cut -d '/' -f 3 <<< "${refs}" | sort -h | tac | head -n 5) )
+
+get_versions() {
+	for index in "${!versions[@]}"; do
+	  echo "  $((index))) ${versions[$index]}"
+	done
+}
+
+query_version() {
+	read -r -p "Pick a version (index): " choice # Read stdin and save the value on the $choice var
+	echo ${choice}
+}
+
+download_package() {
+	version=${versions[${1}]}
+	if [[ -n ${version} ]]; then # Verify if the version with that index exists
+		wget -q --show-progress "https://github.com/CanastaWiki/Canasta-CLI/releases/download/${version}/canasta"
+		echo "Installing Canasta CLI"
+		chmod u=rwx,g=xr,o=x canasta
+		sudo mv canasta /usr/local/bin/canasta
+	else
+		echo "Invalid version"
+	fi
+}
+
+while true; do
+	case "${1}" in
+		-l|--list-versions)
+			get_versions
+			shift
+			;;
+		-i|--install)
+			if [[ -n ${2} ]]; then
+				download_package ${2}
+				shift
+			else
+				get_versions
+				download_package $(query_version)
+			fi
+			shift
+			;;
+		-*)
+			die "Illegal option ${1}"
+			;;
+		*) 	wget -q  --show-progress "https://github.com/CanastaWiki/Canasta-CLI/releases/latest/download/canasta"
+			echo "Installing Canasta CLI"
+	                chmod u=rwx,g=xr,o=x canasta
+        	        mv canasta /usr/local/bin/canasta
+			break
+			;;
+  esac
+done
